b4d732cc71e361802b13e776312b20941720e305
Deepanshu Arya <deepanshu.da85@gmail.com>
2023-07-19 11:39:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static(__dirname + "/public/"))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/css/style.css', (req,res) =>{
+    res.sendFile(__dirname + 'public/css/style.css')
+})
+
+
+app.listen(port,()=>{
+    console.log("Server is running on port 3000")
+})
