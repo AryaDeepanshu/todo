b576e074706d45765fe1389986f387def52ba471
Deepanshu Arya <deepanshu.da85@gmail.com>
2023-07-19 19:26:31 +0530

    saving to file working

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 app.use(express.static(__dirname + "/public/"))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
@@ -14,6 +18,26 @@ app.get('/js/todo.js', (req, res) => {
     res.sendFile(__dirname + "public/js/todo.js")
 })
 
+app.post('/todo', (req, res) => {
+    const todo = req.body
+    saveTodo(todo, (error) => {
+        if(error){
+            console.log(error)
+            res.status(500)
+            
+            res.json({error: error}).send()
+        }else{
+            res.status(200).send()
+        }
+    })
+})
+
+function saveTodo(todo, callback){
+   let stream = fs.createWriteStream("todo.todo", {flags: 'a'})
+   stream.write(JSON.stringify(todo) + "\n")
+   stream.end()
+    callback()
+}
 
 app.listen(port,()=>{
     console.log("Server is running on port 3000")
--- public/js/todo.js
+++ public/js/todo.js
@@ -1,21 +1,50 @@
+const userName = prompt("Enter your Name: ");
+
 const addTodoBtn = document.getElementById("add-btn")
 const todoTextBox = document.getElementById("addNewTodoText")
+
+function saveTodo (todo, isMarked, callback){
+    fetch('/todo', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({text: todo, createdBy : userName, isMarked: isMarked })
+      }).then(function (response) {
+        if(response){
+            callback()
+        }else{
+            callback("Something went wrong")
+        }
+    })
+}
 
 addTodoBtn.addEventListener("click", () => {
     let todoInputText = todoTextBox.value
     if(todoInputText){
-        addTodoToDOM(todoInputText);
+        saveTodo(todoInputText, false, (error) => {
+            if(error){
+                alert("Error: " + error)
+            }else{
+                addTodoToDOM(todoInputText, false)
+            }
+        })
+    }else{
+        alert("Todo text is empty")
     }
 })
 
-function addTodoToDOM(todo){
+function addTodoToDOM(todo, isMarked){
+    if(!todo){
+        return
+    }
+    let status = isMarked? "text-decoration: line-through":""
     const todoList = document.getElementById("todoList")
     const todoItem = document.createElement("li")
     const todoData = document.createElement("span")
     
     const todoItemChkBox = document.createElement("input")
     todoItemChkBox.setAttribute('type', 'checkbox')
-    todoItemChkBox.setAttribute('class', 'isMarked')
+    todoItemChkBox.setAttribute('ismarked', status)
+    todoItemChkBox.checked = status
     
     const todoItemEdtBtn = document.createElement("button")
     todoItemEdtBtn.setAttribute('class', 'edit-btn')
@@ -32,5 +61,5 @@ function addTodoToDOM(todo){
     todoItem.appendChild(todoItemDltBtn)
 
     todoList.appendChild(todoItem)
-
 }
+
